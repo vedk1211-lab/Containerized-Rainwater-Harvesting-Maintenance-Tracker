4de66d4228a16af27fba372322875253ce06095f
Vedant Kulkarni <vedk1211@gmail.com>
2026-09-22 12:30:57 +0530

    Add SystemController and routes

---
+++ src/main/java/com/rwh/tracker/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.rwh.tracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Handles the application home page.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/systems";
+    }
+}
